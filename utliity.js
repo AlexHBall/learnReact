e6bdeb6c4c600db68fb457ac0b951c5bb00b64af
AlexHBall <timmygaines22@gmail.com>
2021-01-07 17:31:36 +0100

    add filter reference

--- utliity.js
+++ utliity.js
@@ -5,3 +5,7 @@ function sayHello(name){
     return `Hello ${name}`;
 }
 
+const grades = [10, 2, 21, 35, 50, -10, 0, 1];
+
+//get all grades > 20
+const result = grades.filter(grade => grade > 20);
